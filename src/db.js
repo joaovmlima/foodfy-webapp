7ff6b88c041ebedeafad80d2608d55d82c1e19a8
João Victor Lima <oi.oaoj@gmail.com>
2020-08-17 19:39:59 -0300

    removed ssl option

--- src/db.js
+++ src/db.js
@@ -1,8 +1,7 @@
 const { Client } = require('pg')
 
 const client = new Client({
-    connectionString: process.env.DATABASE_URL || 'postgresql://postgres:foodfy@localhost:5050/foodfy',
-    ssl: !!process.env.DATABASE_URL
+    connectionString: process.env.DATABASE_URL || 'postgresql://postgres:foodfy@localhost:5050/foodfy'
 })
 
 client.connect()
