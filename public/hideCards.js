faa572856d70d0555a38b9eeb739d70766f46c9b
João Victor <oi.oaoj@gmail.com>
2020-10-07 20:00:18 -0300

    delete file and grouped it on scripts.js

--- public/hideCards.js
+++
@@ -1,9 +0,0 @@
-//Hide recipe cards if there are none
-let cardInfo = document.querySelector('h1 a')
-let cards = document.getElementsByClassName('cards')
-
-if (cardInfo.innerHTML === "") {
-    cards[0].style.visibility = 'hidden'
-    console.log("BUCETA")
-
-}
